f4d1a8ee89135b17b2d4a1900797800915bfd68a
Shaun Bowe <shaunbowe@gmail.com>
2015-04-28 13:37:04 -0500

    Create visibilityChanged.js

---
+++ visibilityChanged.js
@@ -0,0 +1,32 @@
+(function ($) {
+    var defaults = {
+        callback : function () { },
+        runOnLoad: true,
+        frequency: 300
+    };
+
+    var methods = {};
+    methods.checkVisibility = function (element, options) {
+        var previousVisibility = element.data('previousVisibility');
+        var isVisible = element.is(':visible');
+        element.data('previousVisibility', isVisible);
+        if (previousVisibility == null) {
+            if (options.runOnLoad) {
+                options.callback(element, isVisible);
+            }
+        } else if(previousVisibility !== isVisible) {
+            options.callback(element, isVisible);
+        }
+        
+        setTimeout(function() {
+            methods.checkVisibility(element, options);
+        }, options.frequency);
+    };
+
+    $.fn.visibilityChanged = function (options) {
+        options = $.extend(defaults, options);
+        return this.each(function () {
+            methods.checkVisibility($(this), options);
+        });
+    };
+})(jQuery);
